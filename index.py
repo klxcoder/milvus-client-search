d5c86f784362f1f10d39bdb7e47aad1eb61c8175
klxcoder <klxcoder@gmail.com>
2025-03-22 09:17:01 +0700

    client.insert

--- index.py
+++ index.py
@@ -12,5 +12,22 @@ client.create_collection(
     dimension=DIMENSION,
 )
 
+docs = [
+    "Artificial intelligence was founded as an academic discipline in 1956.",
+    "Alan Turing was the first person to conduct substantial research in AI.",
+    "Born in Maida Vale, London, Turing was raised in southern England.",
+]
+
+vectors = encode_queries(docs)
+
+data = [
+    {"id": i, "vector": vectors[i], "text": docs[i], "subject": "history"}
+    for i in range(len(vectors))
+]
+
+res = client.insert(collection_name="demo_collection", data=data)
+
+print(res)
+
 query = input("Enter a query: ")
 print(query.upper())
